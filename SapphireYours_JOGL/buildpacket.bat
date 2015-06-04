f4d9b694196e511ec0b9bc5501592f07ed286291
c0pperdragon <reinhard.grafl@aon.at>
2015-06-04 18:12:44 +0200

    Background busic, gracefull shutdown

--- SapphireYours_JOGL/buildpacket.bat
+++ SapphireYours_JOGL/buildpacket.bat
@@ -3,17 +3,19 @@ mkdir dist
 
 set JAR="C:\Program Files\Java\jdk1.7.0_03\bin\jar.exe"
 %JAR% cfm dist\sapphireyours.jar manifest.txt 
-%JAR% -uf dist\sapphireyours.jar -C ..\SapphireYours_JOGL\bin art 
-%JAR% -uf dist\sapphireyours.jar -C ..\SapphireYours_JOGL\bin gfx
-%JAR% -uf dist\sapphireyours.jar -C ..\SapphireYours_JOGL\bin grafl
-%JAR% -uf dist\sapphireyours.jar -C ..\SapphireYours_JOGL\bin levels
-%JAR% -uf dist\sapphireyours.jar -C ..\SapphireYours_JOGL\bin sound
-%JAR% -uf dist\sapphireyours.jar -C ..\SapphireYours_JOGL\bin Launcher.class
-%JAR% -uf dist\sapphireyours.jar -C ..\SapphireYours_JOGL\bin Launcher$1.class
+%JAR% -uf dist\sapphireyours.jar -C bin grafl
+%JAR% -uf dist\sapphireyours.jar -C bin Launcher.class
+%JAR% -uf dist\sapphireyours.jar -C bin GameCloser.class
 %JAR% -uf dist\sapphireyours.jar -C ..\..\AndroidSimulator\AndroidEmulation_Common\bin javax 
 %JAR% -uf dist\sapphireyours.jar -C ..\..\AndroidSimulator\AndroidEmulation_Common\bin android 
 %JAR% -uf dist\sapphireyours.jar -C ..\..\AndroidSimulator\AndroidEmulation_Common\bin org 
 %JAR% -uf dist\sapphireyours.jar -C ..\..\AndroidSimulator\AndroidEmulation_JOGL\bin android
+%JAR% -uf0 dist\sapphireyours.jar -C ..\SapphireYours_Common\res art 
+%JAR% -uf0 dist\sapphireyours.jar -C ..\SapphireYours_Common\res gfx
+%JAR% -uf dist\sapphireyours.jar -C ..\SapphireYours_Common\res levels
+%JAR% -uf0 dist\sapphireyours.jar -C ..\SapphireYours_Common\res sound
+%JAR% -uf0 dist\sapphireyours.jar -C ..\SapphireYours_Common\res music
 
-mkdir dist\jogamp
-copy ..\..\AndroidSimulator\AndroidEmulation_JOGL\jogl\*.jar dist\jogamp
+mkdir dist\libs
+copy ..\..\AndroidSimulator\AndroidEmulation_JOGL\jogl\*.jar dist\libs
+copy ..\..\AndroidSimulator\AndroidEmulation_JOGL\jaco\*.jar dist\libs
--- SapphireYours_JOGL/src/Launcher.java
+++ SapphireYours_JOGL/src/Launcher.java
@@ -1,12 +1,46 @@
 import android.content.ContextImpl;
 import android.content.SharedPreferencesImpl;
+import android.opengl.GLSurfaceView;
 import android.os.Handler;
 import grafl.sy.game.Game;
 
-public class Launcher {
+public class Launcher {	
+
     public static void main(String[] s)
-   	{	(new Game(new ContextImpl(), new Runnable() { public void run() { System.exit(0); }}, new Handler(), new SharedPreferencesImpl("SapphireYours") ))
-   	       .runWithJOGL("Sapphire Yours 4.0");
+   	{	   		
+   		GameCloser c = new GameCloser();
+   		Game g = new Game(new ContextImpl(), c, new Handler(), new SharedPreferencesImpl("SapphireYours") );
+   		c.setGame(g);
+   	    g.runWithJOGL("Sapphire Yours 4.0");
     }
     
 }
+
+	class GameCloser implements Runnable
+	{
+		boolean triggeredthread;
+		Game game;
+		public GameCloser()
+		{
+			triggeredthread=false;
+			game = null;
+		}
+		public void setGame(Game game)
+		{	
+			this.game = game;
+		}
+		public void run()
+		{
+			if (triggeredthread)
+			{
+				game.setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
+				try { Thread.sleep(200); } catch (InterruptedException e) {}
+				System.exit(0);
+			}
+			else
+			{
+				triggeredthread = true;
+				new Thread(this).start();
+			}
+		}
+	}
