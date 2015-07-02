dfaec5d8f76638362752f9a273bddb2c1a8648b4
c0pperdragon <reinhard.grafl@aon.at>
2015-07-02 20:34:45 +0200

    modified packet builder

--- SapphireYours_JOGL/buildpacket.bat
+++ SapphireYours_JOGL/buildpacket.bat
@@ -4,12 +4,12 @@ mkdir dist
 set JAR="C:\Program Files\Java\jdk1.7.0_03\bin\jar.exe"
 %JAR% cfm dist\sapphireyours.jar manifest.txt 
 %JAR% -uf dist\sapphireyours.jar -C bin grafl
-%JAR% -uf dist\sapphireyours.jar -C bin Launcher.class
-%JAR% -uf dist\sapphireyours.jar -C bin GameCloser.class
+%JAR% -uf dist\sapphireyours.jar -C bin launcher
 %JAR% -uf dist\sapphireyours.jar -C ..\AndroidEmulation_Common\bin javax 
 %JAR% -uf dist\sapphireyours.jar -C ..\AndroidEmulation_Common\bin android 
 %JAR% -uf dist\sapphireyours.jar -C ..\AndroidEmulation_Common\bin org 
 %JAR% -uf dist\sapphireyours.jar -C ..\AndroidEmulation_JOGL\bin android
+%JAR% -uf dist\sapphireyours.jar -C ..\AndroidEmulation_JOGL\bin kuusisto
 %JAR% -uf0 dist\sapphireyours.jar -C ..\SapphireYours_Common\res art 
 %JAR% -uf0 dist\sapphireyours.jar -C ..\SapphireYours_Common\res gfx
 %JAR% -uf dist\sapphireyours.jar -C ..\SapphireYours_Common\res levels
