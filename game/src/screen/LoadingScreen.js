c790662f111686d8a1bc54961b490030610aa231
c0pperdragon <reinhard.grafl@aon.at>
2019-01-11 18:32:20 +0100

    Set level categories

--- game/src/screen/LoadingScreen.js
+++ game/src/screen/LoadingScreen.js
@@ -15,7 +15,16 @@ LoadingScreen.prototype.draw = function()
     var tr = this.game.textRenderer;
     tr.startDrawing (this.game.screenwidth, this.game.screenheight);    
     
-    tr.addString("Loading", 50,50, 50, false, Game.getColorForDifficulty(5), 
-           TextRenderer.WEIGHT_PLAIN);
+	var msg = "Loading...";
+	var h = 50;
+    tr.addString
+	(	msg, 
+		this.game.screenwidth/2 - tr.determineStringWidth(msg,40)/2, 
+		this.game.screenheight/2 - h/2,
+		h, 
+		false, 
+		0xffffffff, 
+		TextRenderer.WEIGHT_PLAIN
+	);
     tr.flush();    
 };
