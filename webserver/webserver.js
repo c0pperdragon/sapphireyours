7cd732602b78d4329f928e23d59de0c5740272d7
c0pperdragon <reinhard.grafl@aon.at>
2019-01-11 19:56:29 +0100

    first public release of browser version

--- webserver/webserver.js
+++ webserver/webserver.js
@@ -2,7 +2,7 @@
 const express = require('express');
 
 var app = express();
-app.use('/', express.static("../game/"));
+app.use('/', express.static("../"));
 app.listen(8080, "");
 
 console.log("Webserver running, open game in browser from http://localhost:8080/launch.html");
